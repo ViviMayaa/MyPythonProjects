b8484d08394e0a031d3976111cdd2b9262e941b5
Viviane Cristina <vivi_ruri@hotmail.com>
2022-08-01 11:11:56 -0300

    Starting project

---
+++ 1 - BandNameGenerator/processor.py
@@ -0,0 +1,6 @@
+
+city = input("Welcome to the band name generator!\n"
+             "What is the name of the city you were born in?\n").capitalize().strip()
+pet = input("What is the name of a pet you've had?\n").capitalize().strip()
+
+print(f"Your band's name is: {city} {pet}")
