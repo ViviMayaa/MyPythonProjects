80541acfa339599d20fd26c8246a6d3cb461e825
Viviane Cristina <vivi_ruri@hotmail.com>
2022-08-02 10:44:22 -0300

    Adding tipcalc

--- 1 - BandNameGenerator/processor.py
+++ 1 - BandNameGenerator/processor.py
@@ -1,6 +1,6 @@
+print("Welcome to the band name generator " + input("What is your name?\n").capitalize().strip() + "!")
 
-city = input("Welcome to the band name generator!\n"
-             "What is the name of the city you were born in?\n").capitalize().strip()
+city = input("What is the name of the city you were born in?\n").capitalize().strip()
 pet = input("What is the name of a pet you've had?\n").capitalize().strip()
 
 print(f"Your band's name is: {city} {pet}")
---
+++ 2 - TipCalculator/processor.py
@@ -0,0 +1,17 @@
+print("Welcome to the Tip Calculator!")
+
+try:
+    bill = float(input("Please type the total bill: \n"))
+    amount_of_people = int(input("How many people to split the bill? \n"))
+    tip = float(input("What percentage tip would you like to give? \n"))
+
+except ValueError:
+    print("Value most be a number.")
+    raise ValueError
+result = (bill + (bill * (tip/100))) / amount_of_people
+
+print(f"Each person should pay: ${round(result, 2)}")
+#or with 00
+result = "{:.2f}".format(result)
+print(f"Each person should pay: ${result}")
+
