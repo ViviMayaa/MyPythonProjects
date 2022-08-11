7c052368e4c76764d4c61c90de7253adddd3a285
Viviane Cristina <vivi_ruri@hotmail.com>
2022-08-11 11:47:57 -0300

    Adding HigherLowerGame

---
+++ 12 - HigherLowerGame/processor.py
@@ -0,0 +1,49 @@
+import art
+import game_data
+import random
+# compare both options and the one higher is the correct one
+print(art.logo)
+
+
+def start():
+    option_a = random.choice(game_data.data)
+    option_b = random.choice(game_data.data)
+    score = 0
+    if option_a == option_b:
+        option_b = random.choice(game_data.data)
+    first_try = True
+    game(option_a, option_b, first_try, score)
+
+
+def game(option_a, option_b, first_try, score):
+    if not first_try:
+        option_a = option_b
+        option_b = random.choice(game_data.data)
+
+    print(f"Compare A: {option_a['name']}, a {option_a['description']} from {option_a['country']}.")
+    print(art.vs)
+    print(f"Against B: {option_b['name']}, a {option_b['description']} from {option_b['country']}\n")
+    player_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    if player_choice == 'A':
+        player_choice = option_a
+        opponent = option_b
+    elif player_choice == 'B':
+        player_choice = option_b
+        opponent = option_a
+    else:
+        print('Please type a available input. ')
+        game(option_a, option_b, first_try, score)
+
+    if player_choice['follower_count'] > opponent['follower_count']:
+        score += 1
+        print(f"You're right! Current score: {score}\n")
+        first_try = False
+        game(option_a, option_b, first_try, score)
+
+    else:
+        print("You got it wrong! ")
+        quit()
+
+
+start()
