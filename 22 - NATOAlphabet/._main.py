def230f3513f72d87893d34397f952b51555daa9
Viviane Cristina <vivi_ruri@hotmail.com>
2022-09-16 15:13:12 -0300

    Adding PasswordManager otimization

--- 22 - NATOAlphabet/._main.py
+++ 22 - NATOAlphabet/._main.py
@@ -1,12 +1,35 @@
 import pandas as pd
+import string
 
 nato_dict_frame = pd.read_csv("nato_phonetic_alphabet.csv")
 # nato_dict = nato_dict_frame.set_index('letter').to_dict()
 # nato_dict = nato_dict['code']
-print(nato_dict_frame.iterrows())
+
+
+def verifying_error(player_answer):
+    try:
+        output_list = [nato_dict[letter] for letter in player_answer]
+    except KeyError as error:
+        raise KeyError(f"Value {error} must be a letter.")
+
+
 nato_dict = {row.letter: row.code for (index, row) in nato_dict_frame.iterrows()}
 result = []
 player_answer = input("Please insert the word: ").upper()
+verifying_error(player_answer)
+
 for word_answer in player_answer:
     result += [nato_dict[element] for element in nato_dict if element in word_answer]
 print(result)
+
+
+# Second way of handling error
+# for word_answer in player_answer:
+#     list_of_letters = ''
+#     for letters in string.ascii_uppercase:
+#         if letters == word_answer:
+#             list_of_letters = word_answer
+#     if list_of_letters == '':
+#         raise ValueError(f"Value {word_answer} must be a letter.")
+#     result += [nato_dict[element] for element in nato_dict if element in word_answer]
+# print(result)
--- 25 - PasswordManagerProject/main.py
+++ 25 - PasswordManagerProject/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 import string
 import random
 from tkinter import messagebox
+import json
+import ast
 
 created_password = ''
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -27,20 +29,92 @@ def generate_password():
 
 
 def save():
+    file_formatted = {}
     if password_entry.get() != "":
-        file = f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n"
+        file_formatted = {
+            website_entry.get():
+            {
+                "email": email_entry.get(),
+                "password": password_entry.get()
+
+            }
+        }
+        # file = f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n"
     else:
-        file = f"{website_entry.get()} | {email_entry.get()} | {created_password}\n"
+        file_formatted = {
+            website_entry.get():
+                {
+                    "email": email_entry.get(),
+                    "password": created_password
+
+                }
+        }
+
+        # file = f"{website_entry.get()} | {email_entry.get()} | {created_password}\n"
 
     messagebox.showinfo(title="Save", message="Information saved.")
 
-    with open("list_passwords.txt", "w") as text:
-        text.write(file)
+    # with open("list_passwords.txt", "a") as text:
+    #     text.write(file)
+    #     website_entry.delete(0, END)
+    #     password_entry.delete(0, END)
+
+    # Using Json
+    # Reading old data to update and add more data later
+    try:
+        with open("list_passwords.json", "r") as text_read:
+            data = json.load(text_read)
+            #     Updating old with new data
+            data.update(file_formatted)
+    except FileNotFoundError:
+        with open("list_passwords.json", "w") as text_write:
+            json.dump(file_formatted, text_write, indent=4)
+    # writing new file in
+    else:
+        with open("list_passwords.json", "w") as text_write:
+            json.dump(data, text_write, indent=4)
+    finally:
         website_entry.delete(0, END)
         password_entry.delete(0, END)
 
 
+# -----------------------------Search----------------------------------#
+
+def search_data():
+    try:
+        with open("list_passwords.json", 'r') as text_read:
+            # String to dict
+            # text_dict = ast.literal_eval(text_read.read())
+
+            # try:
+            #     searched_data = text_dict[website_entry.get()]
+            # except KeyError:
+            #     messagebox.showinfo(title="Error", message="There is no website.")
+            # finally:
+            #     messagebox.showinfo(title="Data Found", message=f"Data found:\n "
+            #                                                     f"Website: {website_entry.get()}\n"
+            #                                                     f"Email: {searched_data['email']}\n"
+            #                                                     f"Password: {searched_data['password']}")
+
+            # Already reads as Dict
+            text_dict = json.load(text_read)
+            website = website_entry.get()
+            if website in text_dict:
+                messagebox.showinfo(title="Data Found", message=f"Data found:\n "
+                                                                f"Website: {website}\n"
+                                                                f"Email: {text_dict[website]['email']}\n"
+                                                                f"Password: {text_dict[website]['password']}")
+            else:
+                messagebox.showinfo(title="Error", message=f"There is no data for {website}.")
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="There is no file.")
+
+
+    print(type(text_dict))
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -66,15 +140,18 @@ email_entry = Entry(width=35)
 email_entry.insert(END, "angela@email.com")
 email_entry.grid(column=1, row=2, columnspan=2)
 
-password_entry = Entry(width=17)
+password_entry = Entry(width=30)
 password_entry.grid(column=1, row=3)
 password_entry.insert(END, created_password)
 
 
-password_button = Button(text="Generate Password", command=generate_password)
-password_button.grid(column=2, row=3)
+password_button = Button(text="Generate Password", command=generate_password, width= 15)
+password_button.grid(column=3, row=3)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_buttom = Button(text="Search", width= 15, command=search_data)
+search_buttom.grid(column=3, row=1, columnspan=3)
 
 window.mainloop()
